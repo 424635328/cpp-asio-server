204fd24e9ebfb8b09e50725b2881f893f3afa496
Tony <424635328@qq.com>
2025-04-22 12:37:54 +0800

    2025-04-22 12:37:50-S.bat

--- s.bat
+++ s.bat
@@ -3,8 +3,8 @@ chcp 65001 > nul
 setlocal
 
 :: 设置 Git 的编码选项
-git config --global core.quotepath false
-git config --global i18n.commitencoding utf-8
+git config core.quotepath false
+git config i18n.commitencoding utf-8
 
 :: 获取时间戳 (使用 PowerShell)
 for /f "delims=" %%a in ('powershell -command "(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"') do set "timestamp=%%a"
@@ -13,16 +13,16 @@ echo Timestamp: %timestamp%
 
 :_main
 echo Pulling...
-git pull || (echo "Git pull 失败" && exit /b 1)
+git pull || (echo "Git pull 失败" && pause && exit /b 1)
 
 echo Adding...
-git add . || (echo "Git add 失败" && exit /b 1)
+git add . || (echo "Git add 失败" && pause && exit /b 1)
 
 echo Committing...
-git commit -m "%timestamp%-S.bat" --no-verify || (echo "Git commit 失败" && exit /b 1)
+git commit -m "%timestamp%-S.bat" --no-verify || (echo "Git commit 失败" && pause && exit /b 1)
 
 echo Pushing...
-git push || (echo "Git push 失败" && exit /b 1)
+git push || (echo "Git push 失败" && pause && exit /b 1)
 
 echo done
 
